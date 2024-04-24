199cdfd8fd0abc4180527e36c10f243e97624675
Carl Alarcon <csalarcon@apper.ph>
2024-04-24 22:44:22 +0800

    edited index.html to have submit buttons

--- addition-backend-service/src/main/java/com/example/demo/AdditionController.java
+++ addition-backend-service/src/main/java/com/example/demo/AdditionController.java
@@ -1,4 +1,7 @@
+package com.example.handlingformsubmission;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
